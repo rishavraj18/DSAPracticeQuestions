ec75078eb081c4614abd5266d9d8f6c700c2ded0
rishavraj <fullstackdev.rishav@gmail.com>
2022-12-14 23:29:19 +0530

    Inverted half pyramid with number solution

---
+++ Patterns/InvertedHalfPyramidWithNumber.java
@@ -0,0 +1,21 @@
+package Patterns;
+
+// Output
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+public class InvertedHalfPyramidWithNumber {
+
+    public static void main(String[] args){
+        int n = 5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i+1; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
