eb8b9771d866620070290fb48580e8e2a3d53916
rishavraj <fullstackdev.rishav@gmail.com>
2022-12-15 14:56:34 +0530

    Solid Rhombus pattern solution

---
+++ Patterns/SolidRhombusPattern.java
@@ -0,0 +1,25 @@
+package Patterns;
+
+// Output:
+//     *****
+//    *****
+//   *****
+//  *****
+// *****
+
+public class SolidRhombusPattern {
+    public static void main(String[] arg){
+        int n = 5;
+        for(int i=1; i<=n; i++){
+            //spaces
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
