ebef26c64eda03841bb36436a851f22f6ed55c10
rishavraj <fullstackdev.rishav@gmail.com>
2022-12-17 16:06:45 +0530

    Pint Diamond pattern solution

---
+++ Patterns/DiamondPattern.java
@@ -0,0 +1,42 @@
+package Patterns;
+
+//Output:
+
+//    *
+//   ***
+//  *****
+// *******
+// *******
+//  *****
+//   ***
+ //   *
+
+public class DiamondPattern {
+    public static void main(String[] arg){
+        int n = 4;
+        //Upper half
+        for(int i=1; i<=n; i++){ 
+            // spaces
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
+            // stars
+            for(int j=1; j<=((2*i-1)); j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //Lower half
+        for(int i=n; i>=1; i--){ 
+            // spaces
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
+            // stars
+            for(int j=1; j<=((2*i-1)); j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
