f7a347f8c41305fce62988bd92442f00337f8a81
rishavraj <fullstackdev.rishav@gmail.com>
2022-12-20 23:58:08 +0530

    Linear search in array solution

---
+++ Array/LinearSearch.java
@@ -0,0 +1,22 @@
+package Array;
+
+// Find Index of 50 in below array
+public class LinearSearch {
+   public static int linearSearch(int numbers[], int key){
+     for(int i=0; i<numbers.length; i++){
+        if(numbers[i] == key){
+            return i;
+        }
+     }
+
+     return -1;
+   }
+
+    public static void main(String[] arg){
+        int numbers[] = {40, 45, 46, 47, 48, 49, 50, 51, 64, 80};
+        int key = 50;
+
+        int index = linearSearch(numbers, key);
+        System.out.println("Key is at Index: "+index);
+    }
+}
