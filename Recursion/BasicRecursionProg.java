3780f14837b7e7013564b816411b3b5ecd33a51d
rishavraj <fullstackdev.rishav@gmail.com>
2023-07-15 14:20:11 +0530

    Basic Recursion program-2

--- Recursion/BasicRecursionProg.java
+++ Recursion/BasicRecursionProg.java
@@ -1,9 +1,10 @@
 package Recursion;
 
 public class BasicRecursionProg {
+    
     public static void printDec(int n)
     {   
-        if(n==1){ // base case
+        if(n==1){ //base case
             System.out.print(n);
             return;
         }
@@ -11,10 +12,22 @@ public class BasicRecursionProg {
         printDec(n-1);
     }
 
+    public static void printInc(int n)
+    {
+        if(n==1){ //base case
+            System.out.print(1+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
+
     public static void main(String args[]){
         int n = 10;
-        printDec(n);
+        //printDec(n);
+        printInc(n);
     }
 }
 
-// Output: 10 9 8 7 6 5 4 3 2 1
+// printDec Output: 10 9 8 7 6 5 4 3 2 1
+// printInc Output: 1 2 3 4 5 6 7 8 9 10 
